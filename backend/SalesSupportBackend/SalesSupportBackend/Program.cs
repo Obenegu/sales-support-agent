d6b5b321dff684adb8d41d237492721d1b72b8b8
Tabot Junior <152695140+Obenegu@users.noreply.github.com>
2025-12-22 07:58:07 +0100

    Configure web host to use dynamic port

--- backend/SalesSupportBackend/SalesSupportBackend/Program.cs
+++ backend/SalesSupportBackend/SalesSupportBackend/Program.cs
@@ -12,6 +12,8 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
+
 // Add services to the container.
 
 //Database
