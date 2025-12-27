54a7d4ed8e2b0ce1b748831e94f780a37dfa7135
Tabot Junior <152695140+Obenegu@users.noreply.github.com>
2025-12-27 08:11:58 +0100

    Uncomment URL binding for web host

--- backend/SalesSupportBackend/SalesSupportBackend/Program.cs
+++ backend/SalesSupportBackend/SalesSupportBackend/Program.cs
@@ -12,7 +12,7 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
+builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
 
 // Add services to the container.
 
