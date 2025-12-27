d753508424cbab2815542a411a6aa1d907089f6a
Obenegu <tabotjunior07@gmail.com>
2025-12-27 07:34:32 +0100

    Update database connection string and improve error handling in Program.cs

--- backend/SalesSupportBackend/SalesSupportBackend/Program.cs
+++ backend/SalesSupportBackend/SalesSupportBackend/Program.cs
@@ -12,14 +12,23 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
+//builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
 
 // Add services to the container.
 
 //Database
+var connectionString =
+	builder.Configuration.GetConnectionString("DefaultConnection")
+	?? Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+	throw new InvalidOperationException("Database connection string is missing.");
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+	options.UseNpgsql(connectionString);
 });
 
 //Authentication
